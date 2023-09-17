e8c55bcea43451ce24a68b800447560b7d47bc99
Pixscl <robinartb@gmail.com>
2023-09-17 16:15:11 +0800

    Updated css include containers and unfinished signup page

--- Event_Evaluation_with_Sentiment_Analysis_System/views.py
+++ Event_Evaluation_with_Sentiment_Analysis_System/views.py
@@ -1,15 +1,13 @@
-
-from datetime import datetime
 from flask import render_template
 from Event_Evaluation_with_Sentiment_Analysis_System import app
 
 @app.route('/')
 @app.route('/login')
 def login():
-    return render_template(
-        'index.html',
-        title='Login Page',
-        year=datetime.now().year,
-    )
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
