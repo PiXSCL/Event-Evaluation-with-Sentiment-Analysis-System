ae9ffa5ddbc0a123a754bd490d86e9037a9221a7
Pixscl <robinartb@gmail.com>
2023-09-27 17:35:46 +0800

    Form filtering added

--- Event_Evaluation_with_Sentiment_Analysis_System/views.py
+++ Event_Evaluation_with_Sentiment_Analysis_System/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 from Event_Evaluation_with_Sentiment_Analysis_System import app
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,11 +13,15 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
+    forms = db.relationship('Form', backref='user', lazy=True)
+
 class Form(db.Model):  
     formid = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255))  
     description = db.Column(db.String(255)) 
     date_created = db.Column(db.DateTime)
+
+    userid = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 @app.route('/')
 @app.route('/login')
@@ -32,12 +36,17 @@ def login_post():
     user = User.query.filter_by(email=email, password=password).first()
 
     if user:
-        return '<script>alert("Login successful!"); window.location.href="/home";</script>'
+        # Store user's email and ID in the session
+        session['user_email'] = user.email
+        session['user_id'] = user.id
+
+        # Redirect to the user's home page
+        return redirect(url_for('home'))
     else:
         return '<script>alert("Invalid account. Please check the email and password you entered."); window.location.href="/login";</script>'
 
-def fetch_form_data():
-    form_data = Form.query.with_entities(Form.formid, Form.title).all()
+def fetch_form_data(user_id):
+    form_data = Form.query.with_entities(Form.formid, Form.title, Form.date_created).filter_by(userid=user_id).order_by(Form.date_created.desc()).all()
     return form_data
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -64,7 +73,13 @@ if __name__ == '__main__':
 
 @app.route('/home')
 def home():
-    form_data = fetch_form_data()
-    return render_template('home.html', form_data=form_data)
+    user_email = session.get('user_email', None)
+    user_id = session.get('user_id', None)
+
+    if user_email:
+        form_data = fetch_form_data(user_id)
+        return render_template('home.html', user_email=user_email, form_data=form_data)
+    else:
+        return redirect(url_for('login'))
 
 
